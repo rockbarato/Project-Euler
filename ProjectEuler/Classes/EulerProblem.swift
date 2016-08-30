d01d3b13d546f69708a019dc6b5a3db670192d28
Felix Ayala <felix1262@gmail.com>
2016-08-30 10:20:44 -0400

    Add elapsed time for problems

--- ProjectEuler/Classes/EulerProblem.swift
+++ ProjectEuler/Classes/EulerProblem.swift
@@ -14,3 +14,19 @@ protocol EulerProblem {
 	
 	func run() -> Int
 }
+
+extension EulerProblem {
+	func evaluate() -> Evaluation {
+		let begin = clock()
+		let result = self.run()
+		let diff = Double(clock() - begin) / Double(CLOCKS_PER_SEC)
+		
+		return Evaluation(problem: self.number, result: result, elapsedTime: diff)
+	}
+}
+
+struct Evaluation {
+	let problem: Int
+	let result: Int
+	let elapsedTime: Double
+}
