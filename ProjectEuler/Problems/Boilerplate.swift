45e0d64e3e7c02c9edb4b4e3756fab10f837841e
Felix Ayala <felix1262@gmail.com>
2016-08-30 00:52:22 -0400

    Add Boilerplate for Problems

---
+++ ProjectEuler/Problems/Boilerplate.swift
@@ -0,0 +1,24 @@
+//
+//  Boilerplate.swift
+//  ProjectEuler
+//
+//  Created by Felix Ayala on 8/30/16.
+//  Copyright © 2016 Felix Ayala. All rights reserved.
+//
+
+import Foundation
+
+/**
+	Problem No: Title
+
+	Description
+*/
+class Problem: EulerProblem {
+	
+	let number: Int = 0
+	
+	func run() -> Int {
+		return 0
+	}
+	
+}
